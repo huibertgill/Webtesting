2ff67408aadeb3551eef8ef1ed746926577f1c11
Huibert Gill <huibert.gill@gmail.com>
2013-09-02 16:34:48 +0200

    bei fehlerhafte Proxy Verbindung werden die Tests abgebrochen
    Signed-off-by: Huibert Gill <huibert.gill@gmail.com>

--- WebTests/pre-test.js
+++ WebTests/pre-test.js
@@ -1,12 +1,22 @@
 // Pre Flight Tests:
 // Proxy verwendbar?
 
+var googleTitle ='Google';
 
 casper.test.begin('Internet und Google verfügbar TEst', 1, function suite(test) {
 	//Zuerst aktuelle Screenshots erfassen
 	casper.start('http://www.google.de', function(){
-		test.assertTitle('Google', 'Check for working Proxy');
+		test.assertTitle(googleTitle, 'Check for working Proxy');
 		
+	});
+	
+	casper.then(function(){
+		if(this.getTitle() != googleTitle) {
+			this.echo('Not the Google Title');
+			this.exit(1);
+		} else {
+			this.echo('Google Title OK');
+		}
 	});
 	
 	casper.run(function() {
