1cc7a935f45df4c228262af638f5549ee2d333ac
Huibert Gill <huibert.gill@gmail.com>
2013-09-02 16:54:48 +0200

    Abbruch bei Proxy Probleme, und Hinweis bei Error
    Signed-off-by: Huibert Gill <huibert.gill@gmail.com>

--- WebTests/pre-test.js
+++ WebTests/pre-test.js
@@ -12,7 +12,8 @@ casper.test.begin('Internet und Google verfügbar TEst', 1, function suite(test)
 	
 	casper.then(function(){
 		if(this.getTitle() != googleTitle) {
-			this.echo('Not the Google Title');
+			this.echo('\n\n Google Title seht falsch aus. Soll[' + googleTitle + '] ist:[' + this.getTitle() + ']\n dies kann passieren wenn der Proxy nicht funktioniert.');
+			this.echo('Am besten einmal das INternet mit IE besuchen Ueber die oben genannte Proxy. "IM IE: menue->extras->Ineternet optionen->Verbindungen');
 			this.exit(1);
 		} else {
 			this.echo('Google Title OK');
