c0b3f952b666343f57a92432cef4ed17530d7eb0
Huibert Gill <huibert.gill@gmail.com>
2013-09-03 13:56:35 +0200

    Logfile stub und Screenshot bei FAILED implementiert
    Signed-off-by: Huibert Gill <huibert.gill@gmail.com>

---
+++ WebTests/test-suite-pause.bat
@@ -0,0 +1,12 @@
+@echo off
+REM casperjs sucht recursiv nach alle JS files in ein verzeichnis, und führt dann alle gefunden Test durch:
+REM sehe: http://docs.casperjs.org/en/latest/testing.html
+if '%CASPERPATHISSET%'=='1' (
+echo "PATH is OK"
+)else (
+echo "Append Casper Files to Path"
+call setup
+)
+echo on
+call casperjs test suite --proxy=10.16.36.38:3128 --pre=pre-test.js --post=post-test.js
+pause
